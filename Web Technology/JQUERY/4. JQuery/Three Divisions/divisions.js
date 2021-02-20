361c813aa24a1ec7214d0b6f3336dc284ae8a426
Adhithya S <56813380+Adhithya-PGK331@users.noreply.github.com>
2021-02-20 19:27:50 +0530

    Delete Web Technology/JQUERY/4. JQuery directory

--- Web Technology/JQUERY/4. JQuery/Three Divisions/divisions.js
+++
@@ -1,5 +0,0 @@
-//WRITE YOUR jQUERY CODE HERE
-$('#button1').click(function(){
-    $("div[name$='DAIntelligence']").css( "background", "yellow" );
-    $("div[name$='ASI_Intelligence']").css( "background", "yellow" );
-});
